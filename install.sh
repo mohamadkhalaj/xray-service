e2d9cb95a8a2c17df60a622f8dd5a615a6d17177
MohammadKhalaj <62938359+mohamadkhalaj@users.noreply.github.com>
2023-07-20 19:55:40 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #!/bin/bash
+cd /tmp
+wget https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service &>/dev/null
+wget https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service.sh &>/dev/null
 chmod +x xray_service xray_service.sh
 mv xray_service.sh /root/
 mv xray_service /etc/init.d
