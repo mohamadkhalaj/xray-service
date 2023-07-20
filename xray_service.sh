8cb10160a6c4165cb913da5d29f684493baba0b6
MohammadKhalaj <62938359+mohamadkhalaj@users.noreply.github.com>
2023-07-20 20:13:52 +0330

    Update xray_service.sh

--- xray_service.sh
+++ xray_service.sh
@@ -2,7 +2,7 @@
 
 service passwall stop
 cd /tmp/
-wget https://github.com/mohamadkhalaj/xray-service/releases/download/v1/xray.ipk
+wget -O xray.ipk https://github.com/mohamadkhalaj/xray-service/releases/download/v1/xray.ipk
 opkg install xray.ipk -d ram
 rm xray.ipk
 service passwall restart
