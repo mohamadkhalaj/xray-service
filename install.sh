a8acee9d9241a0ab4088ef94a61c5b54814e9359
MohammadKhalaj <62938359+mohamadkhalaj@users.noreply.github.com>
2024-04-04 13:32:45 +0330

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,13 +1,15 @@
 #!/bin/bash
-cd /tmp
-echo Downloading xray_service ... #
-wget https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service &>/dev/null
-echo Downloading xray_service.sh ... #
-wget https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service.sh &>/dev/null
-chmod +x xray_service xray_service.sh
-mv xray_service.sh /root/
-mv xray_service /etc/init.d
-cd /etc/init.d
-echo Enabling xray_service ... #
+
+# Downloading xray_service
+echo "Downloading xray_service..."
+wget -q https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service -O /etc/init.d/xray_service
+wget -q https://raw.githubusercontent.com/mohamadkhalaj/xray-service/main/xray_service.sh -O /root/xray_service.sh
+
+# Set executable permissions
+chmod +x /etc/init.d/xray_service /root/xray_service.sh
+
+# Enable xray_service
+echo "Enabling xray_service..."
 service xray_service enable
-echo Finished successfully. #
+
+echo "Finished successfully."
